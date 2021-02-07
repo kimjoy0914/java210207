6133337fa437d32ff5575317e90177361930e969
kimjoy0914 <kimjoy0914@gmail.com>
2021-02-08 05:52:53 +0900

    methodA 추가함.

--- java210207/src/variable/step1/VariableTest.java
+++ java210207/src/variable/step1/VariableTest.java
@@ -2,6 +2,11 @@ package variable.step1;
 
 public class VariableTest {
 	int age = 10;
+	//메소드 추가하기
+	void methodA(int x) {
+		x = 5;
+		System.out.println("x는 "+x);
+	}
 	public static void main(String[] args) {
 		VariableTest vt = new VariableTest();
 		System.out.println("age ==> "+vt.age);
