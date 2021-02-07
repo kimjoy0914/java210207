cd36fe9b9a06eb686c23654ae7933737d3c0ffcb
kimjoy0914 <kimjoy0914@gmail.com>
2021-02-07 21:07:38 +0900

    variable.step1 패키지 추가 및 VariableTest.java

---
+++ java210207/src/variable/step1/VariableTest.java
@@ -0,0 +1,10 @@
+package variable.step1;
+
+public class VariableTest {
+	int age = 10;
+	public static void main(String[] args) {
+		VariableTest vt = new VariableTest();
+		System.out.println("age ==> "+vt.age);
+	}
+
+}
